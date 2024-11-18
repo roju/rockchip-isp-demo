4ab0dfbb588f5d32e4292f0df2ca2642411f9b90
metabenic <metabenic@gmail.com>
2024-11-17 21:54:50 -0800

    build camera_engine_rkaiq if not yet built

--- run-remote.sh
+++ run-remote.sh
@@ -8,6 +8,7 @@ PASSWORD=$(cat "pw.txt")
 DEV_BOARD_USER=radxa
 DEV_BOARD_HOSTNAME=radxa-zero3.local
 DEV_BOARD_CODE_DIR=/home/$DEV_BOARD_USER/rkaiq_test
+RKAIQ_LIB_FILE="$DEV_BOARD_CODE_DIR/camera_engine_rkaiq/build/rkaiq/all_lib/Release/librkaiq.so"
 
 # send all files in local repo to remote dev board
 sshpass -p "$PASSWORD" \
@@ -24,6 +25,12 @@ $SCRIPT_DIR/ $DEV_BOARD_USER@$DEV_BOARD_HOSTNAME:$DEV_BOARD_CODE_DIR
 sshpass -p "$PASSWORD" \
 ssh -tt $DEV_BOARD_USER@$DEV_BOARD_HOSTNAME << EOF
   cd $DEV_BOARD_CODE_DIR
+  if [[ ! -f "$RKAIQ_LIB_FILE" ]]; then
+      cd camera_engine_rkaiq || exit
+      echo "building rkaiq lib"
+      ./build.sh || exit
+      cd ..
+  fi
   mkdir build
   cd build
   cmake .. && make &&  sudo -S <<< $PASSWORD ./rkaiq_test || exit
